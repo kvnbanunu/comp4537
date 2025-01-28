fec8bd9dc18c319cc7f468c723055464c12824bd
kvnbanunu <kvnbanunu@gmail.com>
2025-01-28 14:35:33 -0800

    changed locale

--- labs/3/modules/utils.js
+++ labs/3/modules/utils.js
@@ -1,6 +1,6 @@
 const TEXT = require('../locals/en/en').TEXT;
 
 exports.getDate = function(name) {
-  const d = new Date();
-  return `<p style="color:blue;">${TEXT.Greeting.replace('1', name).replace('2', d.toLocaleTimeString('en-CA'))}</p>`;
+const d = new Date();
+return `<p style="color:blue;">${TEXT.Greeting.replace('1', name).replace('2', d.toLocaleTimeString('en-US'))}</p>`;
 };
